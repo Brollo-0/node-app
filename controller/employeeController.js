f80d7d0f67660c45f5fa43ef1710cea13ef9689a
Gael Garcia <gaelgarciamine@gmail.com>
2025-11-13 11:33:02 -0600

    test to see if code is working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,3 +1,25 @@
+import bodyParser from "body-parser"
+import Employee from "../model/employeeModel.js"
+
+export const create = async(req, res) => {
+    try {
+        const employeeData = new Employee(req, body)
+        const {userName} = employeeData
+        const employeeExists = await Employee.findOne({userName})
+
+        if(employeeExist) {
+            return res.status(200).json({message: "Employee already exists."})
+        }
+
+        const savedEmployee = await employeeData.save();
+        res.status(200).json(savedEmployee)
+        
+    } catch(error) {
+        return res.status(200).json({error: "Internal Server Error"})
+    }
+}
+
+
 export const fetch = async(req, res) => {
     try {
         res.json("Hello World")
